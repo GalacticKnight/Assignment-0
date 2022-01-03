ef4b3c384f243be2c291e10a82b0189e2c8f2861
Vincent <electricplasma411@gmail.com>
2022-01-03 15:47:54 -0500

    titleCaseEdit(title) fixed

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,13 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let ARR = title.toLowerCase().split(' ');
+
+    for (let i = 0; i < ARR.length; i++) {
+        ARR[i] = ARR[i].charAt(0).toUpperCase() + ARR[i].substring(1);
+
+    }
+    let titleBack = ARR.join(' ');
+    return titleBack;
 }
 
 // Do not edit this line;
